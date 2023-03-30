bd1749f8d8f0615ab06b93bc13909fce7666748a
zhoujh <z_jiahuan@outlook.com>
2023-03-31 00:04:11 +0800

    add yolov8 tensorrt inference

--- README.md
+++ README.md
@@ -6,26 +6,39 @@ This repository implemented the inference of yolo detectors with MNN and ONNXRun
 | :--------------: | :-------------------------------------------: | :--: | :--: | :------: |
 |      YOLOx       | https://github.com/Megvii-BaseDetection/YOLOX |  N   |  Y   |    N     |
 |      YOLOv6      |       https://github.com/meituan/YOLOv6       |  N   |  Y   |    Y     |
-|      YOLOv8      |  https://github.com/ultralytics/ultralytics   |  Y   |  Y   |    N     |
+|      YOLOv8      |  https://github.com/ultralytics/ultralytics   |  Y   |  Y   |    Y     |
 | YOLOv7-keypoints |    https://github.com/derronqi/yolov7-face    |  Y   |  N   |    N     |
 
 ## Requirements
 
 - MNN >= 2.0.0
 - ONNXRuntime
+- TensorRT >= 8.2
 
 ## Demo
 
 ```shell
-# MNN inference (Yolov8)
+## MNN inference (Yolov8)
 cd mnn/yolov8/
 mkdir build && cd build
 cmake ..
 make
 ./yolov8_mnn [demo] [model_path] [dataset] [input] [input_size] [show]
 
-# ONNXRuntime inference
+
+## ONNXRuntime inference
 python yolov8_onnx_inference.py DEMO MODEL INPUT [--score_thr SCORE_THR] [--nms_thr NMS_THR] [--input_size INPUT_SIZE] [--classes_txt CLASSES_TXT] [--show]
+
+
+## TensorRT inference
+# you may need to modify the TensorRT path in CMakeLists.txt according to your install path
+# include_directories(/opt/TensorRT-8.2.1.8/include)
+# link_directories(/opt/TensorRT-8.2.1.8/lib)
+cd tensorrt/yolov8/
+mkdir build && cd build
+cmake ..
+make
+./yolov8_trt [demo] [model_path] [dataset] [input] [input_size] [show]
 ```
 
 Yolov8-n inference with image using MNN:
@@ -65,8 +78,30 @@ stop sign: 0.346    [0, 254, 32, 325]
 person: 0.301    [0, 551, 67, 873]
 ```
 
-Yolov8-n inference with stream using ONNXRuntime
+Yolov8-n inference with camera using ONNXRuntime
 
 ```shell
 python yolov8_onnx_inference.py stream /path/to/yolov8n_coco_640.onnx 0 --input_size 640 --show
 ```
+
+Yolov8-n inference with image using TensorRT
+
+```shell
+./yolov8_trt image /path/to/yolov8n_coco_640_fp16.trt coco ../../../assets/bus.jpg 640
+
+# output
+detect 6 objects in 386.102 ms
+person: 0.875488        ([669.938, 376.312], [808.312, 877.5])
+person: 0.868164        ([48.9375, 398.25], [243, 901.125])
+bus: 0.862305   ([20.25, 229.5], [796.5, 754.312])
+person: 0.820312        ([221.062, 405], [344.25, 855.562])
+stop sign: 0.348633     ([0, 253.125], [32.0625, 322.312])
+person: 0.301514        ([0, 550.125], [65.8125, 872.438])
+```
+
+Yolov8-n inference with camera using TensorRT
+
+```shell
+./yolov8_trt stream /path/to/yolov8n_coco_640_fp16.trt coco 0 640 1
+```
+
---
+++ tensorrt/yolov8/src/main.cpp
@@ -0,0 +1,152 @@
+#include "yolov8_trt.hpp"
+
+static const std::vector<std::string> COCO_NAMES = {
+    "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
+    "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
+    "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
+    "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
+    "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
+    "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch",
+    "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone",
+    "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear",
+    "hair drier", "toothbrush" 
+};
+
+#define CUDA_DEVICE 0
+
+
+void image_demo(int argc, char** argv)
+{
+    const std::string imgPath = argv[4];
+    const int size = atoi(argv[5]);
+    bool show = argc == 7? atoi(argv[6]): 0;
+
+    std::vector<std::string> labels;
+    std::string dataset = argv[3];
+    if (dataset == "coco")
+    {
+        labels = COCO_NAMES;
+    }
+    else
+    {
+        std::cout << "unrecognized dataset: "  << argv[2] << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    TRTDetConfigs configs;
+    configs.trtPath = argv[2];
+    configs.inputSize = cv::Size(size, size);
+    configs.classNum = labels.size();
+    configs.scoreThr = 0.25;
+    configs.nmsThr = 0.5;
+    configs.means = {0.0f, 0.0f, 0.0f};
+    configs.norms = {255.0f, 255.0f, 255.0f};
+
+    Yolov8TRT yolo(configs);
+
+    cv::Mat image = cv::imread(imgPath);
+    std::vector<Object> objects;
+    auto start = std::chrono::steady_clock::now();
+    yolo.detect(image, objects);
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    float timeMs = elapsed.count()*1000;
+    std::cout << "detect " << (int)objects.size() << " objects in " <<  timeMs << " ms\n";
+    for (const auto& obj: objects)
+    {
+        std::cout << labels[obj.label] << ": " << obj.prob << "\t";
+        std::cout << "(" << obj.rect.tl() << ", " << obj.rect.br() << ")" << std::endl; 
+    }
+    if (show)
+    {
+        yolo.drawObject(image, objects, labels);
+        cv::imshow("result", image);
+        cv::waitKey(0);
+    }
+}
+
+
+void stream_demo(int argc, char** argv)
+{
+    const int camId = atoi(argv[4]);
+    const int size = atoi(argv[5]);
+    bool show = argc == 7? atoi(argv[6]): 0;
+
+    std::vector<std::string> labels;
+    std::string dataset = argv[3];
+    if (dataset == "coco")
+    {
+        labels = COCO_NAMES;
+    }
+    else
+    {
+        std::cout << "unrecognized dataset: "  << argv[2] << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    TRTDetConfigs configs;
+    configs.trtPath = argv[2];
+    configs.inputSize = cv::Size(size, size);
+    configs.classNum = labels.size();
+    configs.scoreThr = 0.25;
+    configs.nmsThr = 0.5;
+    configs.means = {0.0f, 0.0f, 0.0f};
+    configs.norms = {255.0f, 255.0f, 255.0f};
+
+    Yolov8TRT yolo(configs);
+
+    cv::Mat image;
+    cv::VideoCapture cap(camId);
+    cap.set(cv::CAP_PROP_FPS, 30);
+    cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+
+    std::vector<Object> objects;
+    while (true)
+    {
+        cap >> image;
+        objects.clear();
+        auto start = std::chrono::steady_clock::now();
+        yolo.detect(image, objects);
+        auto end = std::chrono::steady_clock::now();
+        std::chrono::duration<double> elapsed = end - start;
+        float timeMs = elapsed.count()*1000;
+        std::cout << "detect " << (int)objects.size() << " objects in " <<  timeMs << " ms\n";
+
+        if (show)
+        {
+            yolo.drawObject(image, objects, labels);
+            cv::imshow("result", image);
+            if (cv::waitKey(1) == 'q')
+            {
+                break;
+            }
+        }
+    }
+}
+
+
+int main(int argc, char** argv)
+{
+    if (argc != 6 && argc != 7)
+    {
+        fprintf(stderr, "usage: %s [demo] [model_path] [dataset] [input] [input_size] [show] ..\n", argv[0]);
+        return -1;
+    }
+    cudaSetDevice(CUDA_DEVICE);
+    std::string demo = argv[1];
+    if (demo == "image")
+    {
+        image_demo(argc, argv);
+    }
+    else if (demo == "stream")
+    {
+        stream_demo(argc, argv);
+    }
+    else
+    {
+        std::cout << "unrecognized demo type: "  << argv[1] << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    return 0;
+}
